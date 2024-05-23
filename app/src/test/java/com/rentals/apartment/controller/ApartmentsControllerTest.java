d9c29144c822a6109adea1dc1b436353f0bc6ee0
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2024-05-22 21:06:27 -0600

    Add unit testing

---
+++ app/src/test/java/com/rentals/apartment/controller/ApartmentsControllerTest.java
@@ -0,0 +1,28 @@
+package com.rentals.apartment.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApartmentsControllerTest {
+
+    @Test
+    void getAllApartmentsWithSpecifications() {
+    }
+
+    @Test
+    void getAllApartments() {
+    }
+
+    @Test
+    void getApartment() {
+    }
+
+    @Test
+    void createApartment() {
+    }
+
+    @Test
+    void editApartment() {
+    }
+}
